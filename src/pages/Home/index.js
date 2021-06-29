c8f80f9cd17ce60bfa38d38e92e2d8ae9762cf7e
gayapedro <pedrogaya@gmail.com>
2021-06-28 21:24:34 -0300

    style login page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,8 @@
-import { TextField, Button } from '@material-ui/core';
+import {
+  TextField, Button, Snackbar, Backdrop, CircularProgress,
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
@@ -6,59 +10,106 @@ import qs from 'querystring';
 import crypto from 'crypto';
 import useStyles from './style';
 import useAuth from '../../hooks/useAuth';
+import './style.css';
 
 function Home() {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const { logar } = useAuth();
   const classes = useStyles();
   const history = useHistory();
   const { register, handleSubmit } = useForm();
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setError(false);
+    }, 3000);
+
+    return () => clearTimeout(timeoutId);
+  }, [error]);
+
   const onSubmit = async (data) => {
-    console.log(data);
+    setError(false);
+    setErrorMessage('');
+    setLoading(true);
     const now = String(Math.floor(new Date().getTime() / 1000));
     const host = 'https://www.mercadobitcoin.net';
     const endpoint = '/tapi/v3/';
     const query = { tapi_method: 'get_account_info', tapi_nonce: now };
-    const queryString = qs.stringify(query);
-    const h = crypto.createHmac('sha512', data.secret)
-      .update(`${endpoint}?${queryString}`)
-      .digest('hex');
 
-    const config = {
-      headers: {
-        'TAPI-ID': data.id,
-        'TAPI-MAC': h,
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    };
     try {
+      const queryString = qs.stringify(query);
+      const h = crypto.createHmac('sha512', data.secret)
+        .update(`${endpoint}?${queryString}`)
+        .digest('hex');
+
+      const config = {
+        headers: {
+          'TAPI-ID': data.id,
+          'TAPI-MAC': h,
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      };
       const response = await axios.post(`${host}${endpoint}`, queryString, config);
       if (response.data.status_code !== 100) {
-        console.log('erro');
-        // exibir erro
+        setError(true);
+        setErrorMessage('Client ID e Secret não conferem.');
+        setLoading(false);
         return response;
       }
       logar(data.id, data.secret, () => history.push('/'));
-    } catch (error) {
-      throw error.message;
+    } catch {
+      setError(true);
+      setErrorMessage('Client ID e Secret não conferem.');
     }
+    setLoading(false);
     return '';
   };
 
   return (
-    <form
-      className={classes.root}
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <TextField color="disabled" className={classes.input} id="standard-basic" label="Client ID" {...register('id')} />
-      <TextField id="standard-basic" label="Secret ID" {...register('secret')} />
-      <Button className={classes.button} type="submit" variant="contained" color="primary">
-        Entrar
-      </Button>
-    </form>
+    <>
+      <form
+        className={classes.rootForm}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <img src="/logo.png" alt="Gaya bot" />
+        <TextField
+          InputLabelProps={{
+            classes: {
+              root: classes.cssLabel,
+            },
+          }}
+          id="standard-basic"
+          label="Client ID"
+          {...register('id')}
+        />
+        <TextField
+          InputLabelProps={{
+            classes: {
+              root: classes.cssLabel,
+            },
+          }}
+          id="standard-basic"
+          label="Secret ID"
+          {...register('secret')}
+        />
+        <Button className={classes.button} type="submit" variant="contained" color="primary">
+          Entrar
+        </Button>
+        <Snackbar className={classes.alert} open={error} autoHideDuration={6000}>
+          <Alert severity="error">
+            {errorMessage}
+          </Alert>
+        </Snackbar>
+      </form>
+      <Backdrop className="backdrop" open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
   );
 }
 
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  rootForm: {
     '& > *': {
       margin: theme.spacing(1),
       width: '25ch',
@@ -11,10 +11,6 @@ const useStyles = makeStyles((theme) => ({
     placeContent: 'center',
     gap: 15,
   },
-  input: {
-    color: 'red',
-
-  },
   button: {
     margin: 'auto',
     width: '95%',
@@ -22,6 +18,26 @@ const useStyles = makeStyles((theme) => ({
     '&:hover': {
       backgroundColor: '#999999',
     },
+  },
+  cssLabel: {
+    color: '#333333 !important',
+  },
+  alert: {
+    width: '100%',
+    '& > * + *': {
+      margin: theme.spacing(2),
+    },
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
 }));
 
